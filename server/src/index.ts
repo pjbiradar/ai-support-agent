5c1390fe402b0b9cb3ca45771664e0554d32cf63
pooja biradar <poojabiradar2412@gmail.com>
2026-05-13 17:00:20 +0530

    connected mongodb to express server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,16 @@
 import express from "express";
 import cors from 'cors'
 import dotenv from 'dotenv';
+import mongoose from "mongoose";
 
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 8000;
+// const MONGODB_URL = process.env.MONGODB_URL || ''
+// console.log('MONGODB_URI:', process.env.MONGODB_URI)
+const MONGODB_URI = (process.env.MONGODB_URI || '').replace(/['"]+/g, '').trim()
 
 
 app.use(cors({
@@ -16,11 +20,22 @@ app.use(cors({
 app.use(express.json())
 
 app.get('/health',(req,res)=>{
-    res.json({status: 'server is running'})
+    res.json({
+        status: 'server is running',
+        database:mongoose.connection.readyState === 1 ? "connected" : "disconnected"
+})
+
+})
+mongoose.connect(MONGODB_URI).then(()=>{
+    console.log("MongoDB connected successfully")
+    app.listen(PORT,()=>{
+        console.log(`Server running on port ${PORT}`)
+    })
+}).catch((error)=>{
+    console.log('MongoDB connection failed:', error)
+    process.exit(1)
 
 })
 
-app.listen(PORT,()=>{
-    console.log(`Server running on port ${PORT}`)
-})
+
 
